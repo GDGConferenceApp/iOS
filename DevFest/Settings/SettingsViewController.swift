c49377fcb008f4a1c3b198ab59b256abcea841c3
Brendon Justin <brendonjustin@gmail.com>
2017-01-08 15:37:17 -0600

    Add an empty settings view controller

---
+++ DevFest/Settings/SettingsViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SettingsViewController.swift
+//  DevFest
+//
+//  Created by Brendon Justin on 1/8/17.
+//  Copyright © 2017 GDGConferenceApp. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UITableViewController {
+}
